a9c03302bb3f76b5ef56950f0a0166ab022a6563
Vendicated <vendicated@riseup.net>
2021-06-22 23:39:57 +0200

    chore: format code

--- components/TextContainerButton.jsx
+++ components/TextContainerButton.jsx
@@ -1,44 +1,46 @@
-const { React, getModuleByDisplayName } = require('powercord/webpack')
-const { Button } = require('powercord/components')
+const { React, getModuleByDisplayName } = require("powercord/webpack");
+const { Button } = require("powercord/components");
 
 const Tooltip = getModuleByDisplayName("Tooltip", false);
 
 module.exports = class TextContainerButton extends React.PureComponent {
-	constructor(props){
-			super(props)
-			this.get = this.props.settings.get
-			this.set = this.props.settings.set
-			this.enabled = this.get('nazify', true)
-	}
+  constructor(props) {
+    super(props);
+    this.get = this.props.settings.get;
+    this.set = this.props.settings.set;
+    this.enabled = this.get("nazify", true);
+  }
 
-	render() {
-			return <>
-        <Tooltip text={`${this.enabled ? 'Disable' : 'Enable'} Grammar Nazi`} position='top'>
-        {({ onMouseLeave, onMouseEnter }) => (
-          <Button
-            className={`toggle-button ${this.enabled ? 'active' : 'inactive'}`}
-            look={Button.Looks.BLANK}
-            size={Button.Sizes.ICON}
-            onClick={()=>{
-              this.enabled = !this.enabled
-              this.set('nazify', this.enabled)
-              this.forceUpdate()
-            }}
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}  
-          >
-            <div className='contents-18-Yxp button-3AYNKb button-318s1X'>
-              <svg className='buttonWrapper-1ZmCpA' width='24px' height='24px'>
-                <polygon
-                  fill='currentColor'
-                  transform='translate(-4,-4.4), scale(0.9)'
-                  points='21.1059,7.71462 10.3929,18.4277 18.4277,26.4625 26.4624,18.4277 23.7842,15.7494 18.4277,21.106 15.7494,18.4277 23.7841,10.3929 31.819,18.4277 18.4277,31.8191 5.03626,18.4277 18.4277,5.03633 '
-                />
-              </svg>
-            </div>
-          </Button>
-        )}
-			  </Tooltip>
+  render() {
+    return (
+      <>
+        <Tooltip text={`${this.enabled ? "Disable" : "Enable"} Grammar Nazi`} position="top">
+          {({ onMouseLeave, onMouseEnter }) => (
+            <Button
+              className={`toggle-button ${this.enabled ? "active" : "inactive"}`}
+              look={Button.Looks.BLANK}
+              size={Button.Sizes.ICON}
+              onClick={() => {
+                this.enabled = !this.enabled;
+                this.set("nazify", this.enabled);
+                this.forceUpdate();
+              }}
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
+            >
+              <div className="contents-18-Yxp button-3AYNKb button-318s1X">
+                <svg className="buttonWrapper-1ZmCpA" width="24px" height="24px">
+                  <polygon
+                    fill="currentColor"
+                    transform="translate(-4,-4.4), scale(0.9)"
+                    points="21.1059,7.71462 10.3929,18.4277 18.4277,26.4625 26.4624,18.4277 23.7842,15.7494 18.4277,21.106 15.7494,18.4277 23.7841,10.3929 31.819,18.4277 18.4277,31.8191 5.03626,18.4277 18.4277,5.03633 "
+                  />
+                </svg>
+              </div>
+            </Button>
+          )}
+        </Tooltip>
       </>
-	}
-}
+    );
+  }
+};
--- index.js
+++ index.js
@@ -1,191 +1,197 @@
-const { Plugin }  = require('powercord/entities')
-const { inject, uninject } = require('powercord/injector')
-const { getModule: getMod, messages, getModuleByDisplayName, React } = require('powercord/webpack')
+const { Plugin } = require("powercord/entities");
+const { inject, uninject } = require("powercord/injector");
+const { getModule: getMod, messages, getModuleByDisplayName, React } = require("powercord/webpack");
 const getModule = (key, sync) => getMod(typeof key === "string" ? [key] : key, sync);
-const { findInReactTree } = require('powercord/util')
-const { receiveMessage } = messages
+const { findInReactTree } = require("powercord/util");
+const { receiveMessage } = messages;
 
-const HeaderBarButton = require('./components/HeaderBarButton')
-const TextContainerButton = require('./components/TextContainerButton')
+const HeaderBarButton = require("./components/HeaderBarButton");
+const TextContainerButton = require("./components/TextContainerButton");
 
 module.exports = class GrammarNazi extends Plugin {
   async startPlugin() {
-      powercord.api.settings.registerSettings("GrammarNazi", {
-        category: this.entityID,
-        label: "GrammarNazi",
-        render: require('./components/Settings')
-      });
-  
-  
+    powercord.api.settings.registerSettings("GrammarNazi", {
+      category: this.entityID,
+      label: "GrammarNazi",
+      render: require("./components/Settings")
+    });
+
     powercord.api.commands.registerCommand({
-      command: 'addword',
-      aliases: ['aw', 'aword'],
-			description: 'Add a key/value pair to the custom dictionary.',
-			usage: '{c} "key" "value"',
-			executor: (args) => this.addDict(args)
-    })
+      command: "addword",
+      aliases: ["aw", "aword"],
+      description: "Add a key/value pair to the custom dictionary.",
+      usage: '{c} "key" "value"',
+      executor: args => this.addDict(args)
+    });
     powercord.api.commands.registerCommand({
-      command: 'removeword',
-      aliases: ['rm', 'rmword'],
-			description: 'Remove a key/value pair from the custom dictionary.',
-			usage: '{c} "key"',
-			executor: (args) => this.removeDict(args)
-		})
-		powercord.api.commands.registerCommand({
-      command: 'listwords',
-      aliases: ['lw', 'dictionary', 'dict'],
-			description: 'View the current custom dictionary.',
-			usage: '{c}',
-			executor: () => this.viewDict()
-    })
+      command: "removeword",
+      aliases: ["rm", "rmword"],
+      description: "Remove a key/value pair from the custom dictionary.",
+      usage: '{c} "key"',
+      executor: args => this.removeDict(args)
+    });
+    powercord.api.commands.registerCommand({
+      command: "listwords",
+      aliases: ["lw", "dictionary", "dict"],
+      description: "View the current custom dictionary.",
+      usage: "{c}",
+      executor: () => this.viewDict()
+    });
 
     /* Stylesheet */
-    this.loadStylesheet("style.css")
+    this.loadStylesheet("style.css");
 
     /* Define Settings */
-    if (this.settings.get('customDictionary') === undefined) this.settings.set('customDictionary', {})
-    let settingsArray = ['nazify', 'punctuation', 'capitalization', 'dictionary', 'location']
+    if (this.settings.get("customDictionary") === undefined) this.settings.set("customDictionary", {});
+    let settingsArray = ["nazify", "punctuation", "capitalization", "dictionary", "location"];
     for (let i = 0; i < settingsArray.length; i++) {
-      if (this.settings.get(settingsArray[i]) === undefined) this.settings.set(settingsArray[i], false)
+      if (this.settings.get(settingsArray[i]) === undefined) this.settings.set(settingsArray[i], false);
     }
 
     /* Inject on Message Send */
-		inject('message-send', messages, 'sendMessage', (args) => {
-      let text = args[1].content.trim()
-      let split = text.split(' ')
-      let customDictionary = this.settings.get('customDictionary')
+    inject(
+      "message-send",
+      messages,
+      "sendMessage",
+      args => {
+        let text = args[1].content.trim();
+        let split = text.split(" ");
+        let customDictionary = this.settings.get("customDictionary");
 
-      if (text.indexOf('```') === -1 && this.settings.get('nazify') && text.charAt(0) !== '.') {
-      if (this.settings.get('dictionary')) text = split.map(c => c in customDictionary ? customDictionary[c] : c).join(' ')
-      if (this.settings.get('punctuation') && (/[a-z0-9]$/gmi).test(text) && split[split.length-1].indexOf('http') === -1 ) text += '.'
-      if (this.settings.get('capitalization') && text.indexOf('http') != 0) text = text.charAt(0).toUpperCase() + text.substring(1)}
+        if (text.indexOf("```") === -1 && this.settings.get("nazify") && text.charAt(0) !== ".") {
+          if (this.settings.get("dictionary")) text = split.map(c => (c in customDictionary ? customDictionary[c] : c)).join(" ");
+          if (this.settings.get("punctuation") && /[a-z0-9]$/gim.test(text) && split[split.length - 1].indexOf("http") === -1) text += ".";
+          if (this.settings.get("capitalization") && text.indexOf("http") != 0) text = text.charAt(0).toUpperCase() + text.substring(1);
+        }
 
-      args[1].content = text
-      return args
-    }, true)
+        args[1].content = text;
+        return args;
+      },
+      true
+    );
 
     /* Inject Toggle Button */
-    const ChannelTextAreaContainer = getModule((m) => m.type && m.type.render && m.type.render.displayName === 'ChannelTextAreaContainer', false)
-    inject('chat-button', ChannelTextAreaContainer.type, 'render', (args, res) => {
-      if (this.settings.get('location') === 'channel-text-area-container') {
-        const props = findInReactTree(res, (r) => r && r.className && r.className.indexOf('buttons-') == 0)
-        props.children.unshift(React.createElement(TextContainerButton, { settings: this.settings }))
+    const ChannelTextAreaContainer = getModule(m => m.type && m.type.render && m.type.render.displayName === "ChannelTextAreaContainer", false);
+    inject("chat-button", ChannelTextAreaContainer.type, "render", (args, res) => {
+      if (this.settings.get("location") === "channel-text-area-container") {
+        const props = findInReactTree(res, r => r && r.className && r.className.indexOf("buttons-") == 0);
+        props.children.unshift(React.createElement(TextContainerButton, { settings: this.settings }));
       }
-        return res
-    })
-    ChannelTextAreaContainer.type.render.displayName = 'ChannelTextAreaContainer'
-    
-    const HeaderBarContainer = await getModuleByDisplayName('HeaderBarContainer')
-    inject('header-bar', HeaderBarContainer.prototype, 'render', (args, res)=> {
-      if (this.settings.get('location') === 'header-bar-container')
-        res.props.toolbar.props.children.unshift(React.createElement(HeaderBarButton, { settings: this.settings, bartype: HeaderBarContainer.Icon }))
-      return res
-    })
-}
+      return res;
+    });
+    ChannelTextAreaContainer.type.render.displayName = "ChannelTextAreaContainer";
 
-    pluginWillUnload() {
-      powercord.api.commands.unregisterCommand('addword')
-      powercord.api.commands.unregisterCommand('removeword')
-      powercord.api.commands.unregisterCommand('listwords')
-      powercord.api.settings.unregisterSettings("GrammarNazi")
-      uninject('message-send')
-      uninject('chat-button')
-      uninject('header-bar')
-      document.querySelectorAll('.toggle-button').forEach(e => e.style.display = 'none')
+    const HeaderBarContainer = await getModuleByDisplayName("HeaderBarContainer");
+    inject("header-bar", HeaderBarContainer.prototype, "render", (args, res) => {
+      if (this.settings.get("location") === "header-bar-container")
+        res.props.toolbar.props.children.unshift(React.createElement(HeaderBarButton, { settings: this.settings, bartype: HeaderBarContainer.Icon }));
+      return res;
+    });
+  }
+
+  pluginWillUnload() {
+    powercord.api.commands.unregisterCommand("addword");
+    powercord.api.commands.unregisterCommand("removeword");
+    powercord.api.commands.unregisterCommand("listwords");
+    powercord.api.settings.unregisterSettings("GrammarNazi");
+    uninject("message-send");
+    uninject("chat-button");
+    uninject("header-bar");
+    document.querySelectorAll(".toggle-button").forEach(e => (e.style.display = "none"));
+  }
+
+  async addDict(args) {
+    /* Custom Bot Attributes */
+    const { BOT_AVATARS } = await getModule("BOT_AVATARS");
+    const { createBotMessage } = await getModule("createBotMessage");
+    const { getChannelId } = getModule("getLastSelectedChannelId", false);
+
+    const receivedMessage = createBotMessage(getChannelId(), {});
+
+    BOT_AVATARS.GrammarNaziAvatar = "https://i.imgur.com/wUcHvh0.png";
+    receivedMessage.author.username = "Grammar Nazi";
+    receivedMessage.author.avatar = "GrammarNaziAvatar";
+
+    /* String Formatting */
+    let newargs = [];
+    let text = args.join(" ");
+    newargs[0] = text.substring(0, text.indexOf('" "')).replace(/"/g, "");
+    newargs[1] = text.substring(text.indexOf('" "') + 2, text.length).replace(/"/g, "");
+    if (newargs[0].length < 1 || newargs[1] == " ") {
+      receivedMessage.content = "Insufficent arguments; both a keyword and value must be supplied.";
+      return receiveMessage(receivedMessage.channel_id, receivedMessage);
     }
 
-    async addDict(args) {
-      /* Custom Bot Attributes */
-      const { BOT_AVATARS } = await getModule('BOT_AVATARS')
-      const { createBotMessage } = await getModule('createBotMessage')
-      const { getChannelId } = getModule('getLastSelectedChannelId', false)
-
-      const receivedMessage = createBotMessage(getChannelId(), {})
-
-      BOT_AVATARS.GrammarNaziAvatar = 'https://i.imgur.com/wUcHvh0.png'
-      receivedMessage.author.username = 'Grammar Nazi'
-      receivedMessage.author.avatar = 'GrammarNaziAvatar'
-
-      /* String Formatting */
-      let newargs = []
-      let text = args.join(' ')
-      newargs[0] = text.substring(0, text.indexOf('" "')).replace(/"/g, '')
-      newargs[1] = text.substring(text.indexOf('" "') + 2, text.length).replace(/"/g, '')
-      if (newargs[0].length < 1 || newargs[1] == ' ') {
-        receivedMessage.content = 'Insufficent arguments; both a keyword and value must be supplied.'
-        return receiveMessage(receivedMessage.channel_id, receivedMessage)
-      }
-
-      /* Duplicate Check */
-      let customDictionary = this.settings.get('customDictionary')
-      if (newargs[0] in customDictionary) {
-        receivedMessage.content = `Entry "${newargs[0]}" already exists!`
-        return receiveMessage(receivedMessage.channel_id, receivedMessage)
-      }
-
-      /* Save to Dictionary */
-      customDictionary[newargs[0]] = newargs[1]
-      this.settings.set('customDictionary', customDictionary)
-
-      receivedMessage.content = `Entry "${newargs[0]}" successfully created with value of "${newargs[1]}".`
-      return receiveMessage(receivedMessage.channel_id, receivedMessage)
+    /* Duplicate Check */
+    let customDictionary = this.settings.get("customDictionary");
+    if (newargs[0] in customDictionary) {
+      receivedMessage.content = `Entry "${newargs[0]}" already exists!`;
+      return receiveMessage(receivedMessage.channel_id, receivedMessage);
     }
 
-    async removeDict(args) {
-      /* Custom Bot Attributes */
-      const { BOT_AVATARS } = await getModule('BOT_AVATARS')
-      const { createBotMessage } = await getModule('createBotMessage')
-      const { getChannelId } = getModule('getLastSelectedChannelId', false)
+    /* Save to Dictionary */
+    customDictionary[newargs[0]] = newargs[1];
+    this.settings.set("customDictionary", customDictionary);
 
-      const receivedMessage = createBotMessage(getChannelId(), {})
+    receivedMessage.content = `Entry "${newargs[0]}" successfully created with value of "${newargs[1]}".`;
+    return receiveMessage(receivedMessage.channel_id, receivedMessage);
+  }
 
-      BOT_AVATARS.GrammarNaziAvatar = 'https://i.imgur.com/wUcHvh0.png'
-      receivedMessage.author.username = 'Grammar Nazi'
-      receivedMessage.author.avatar = 'GrammarNaziAvatar'
+  async removeDict(args) {
+    /* Custom Bot Attributes */
+    const { BOT_AVATARS } = await getModule("BOT_AVATARS");
+    const { createBotMessage } = await getModule("createBotMessage");
+    const { getChannelId } = getModule("getLastSelectedChannelId", false);
 
-      /* String Formatting */
-      let customDictionary = this.settings.get('customDictionary')
-      let text = args.join(' ').replace(/"/gm, '')
+    const receivedMessage = createBotMessage(getChannelId(), {});
 
-      /* Arguments Check */
-      if (!(args.join(' ').includes('"'))) {
-        receivedMessage.content = 'Insufficent arguments; please provide a keyword that exists in the dictionary.'
-        return receiveMessage(receivedMessage.channel_id, receivedMessage)
-      }
+    BOT_AVATARS.GrammarNaziAvatar = "https://i.imgur.com/wUcHvh0.png";
+    receivedMessage.author.username = "Grammar Nazi";
+    receivedMessage.author.avatar = "GrammarNaziAvatar";
 
-      /* Remove from Dictionary */
-      if (text in customDictionary) {
-        delete customDictionary[text]
-        this.settings.set('customDictionary', customDictionary)
-        receivedMessage.content = `Entry ${args[0]} was successfully deleted!`
-        return receiveMessage(receivedMessage.channel_id, receivedMessage)
-      } else {
-        receivedMessage.content = `Entry ${args[0]} does not exist.`
-        return receiveMessage(receivedMessage.channel_id, receivedMessage)
-      }
+    /* String Formatting */
+    let customDictionary = this.settings.get("customDictionary");
+    let text = args.join(" ").replace(/"/gm, "");
+
+    /* Arguments Check */
+    if (!args.join(" ").includes('"')) {
+      receivedMessage.content = "Insufficent arguments; please provide a keyword that exists in the dictionary.";
+      return receiveMessage(receivedMessage.channel_id, receivedMessage);
     }
 
-	  async viewDict() {
-      /* Custom Bot Attributes */
-      const { BOT_AVATARS } = await getModule('BOT_AVATARS')
-      const { createBotMessage } = await getModule('createBotMessage')
-      const { getChannelId } = getModule('getLastSelectedChannelId', false)
-
-      const receivedMessage = createBotMessage(getChannelId(), {})
-
-      BOT_AVATARS.GrammarNaziAvatar = 'https://i.imgur.com/wUcHvh0.png'
-      receivedMessage.author.username = 'Grammar Nazi'
-      receivedMessage.author.avatar = 'GrammarNaziAvatar'
-
-      /* Write Message */
-      let customDictionary = this.settings.get('customDictionary')
-      let dictionary = '> '
-
-      for (let i in customDictionary) {
-        dictionary += i + ' : ' + customDictionary[i] + '\n> '
-      }
-
-      receivedMessage.content = dictionary
-      return receiveMessage(receivedMessage.channel_id, receivedMessage)
+    /* Remove from Dictionary */
+    if (text in customDictionary) {
+      delete customDictionary[text];
+      this.settings.set("customDictionary", customDictionary);
+      receivedMessage.content = `Entry ${args[0]} was successfully deleted!`;
+      return receiveMessage(receivedMessage.channel_id, receivedMessage);
+    } else {
+      receivedMessage.content = `Entry ${args[0]} does not exist.`;
+      return receiveMessage(receivedMessage.channel_id, receivedMessage);
     }
-}
+  }
+
+  async viewDict() {
+    /* Custom Bot Attributes */
+    const { BOT_AVATARS } = await getModule("BOT_AVATARS");
+    const { createBotMessage } = await getModule("createBotMessage");
+    const { getChannelId } = getModule("getLastSelectedChannelId", false);
+
+    const receivedMessage = createBotMessage(getChannelId(), {});
+
+    BOT_AVATARS.GrammarNaziAvatar = "https://i.imgur.com/wUcHvh0.png";
+    receivedMessage.author.username = "Grammar Nazi";
+    receivedMessage.author.avatar = "GrammarNaziAvatar";
+
+    /* Write Message */
+    let customDictionary = this.settings.get("customDictionary");
+    let dictionary = "> ";
+
+    for (let i in customDictionary) {
+      dictionary += i + " : " + customDictionary[i] + "\n> ";
+    }
+
+    receivedMessage.content = dictionary;
+    return receiveMessage(receivedMessage.channel_id, receivedMessage);
+  }
+};
